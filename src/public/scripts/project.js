e8978899b26fe79772d57591f4768972b86d6477
Joel Kuiper <me@joelkuiper.eu>
2015-02-03 14:26:26 +0100

    Remove files with references to projects

--- src/public/scripts/project.js
+++
@@ -1,6 +0,0 @@
-/* -*- tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2; js-indent-level: 2; -*- */
-'use strict';
-
-require(['./common'], function (common) {
-  require(['dispatchers/project']);
-});
